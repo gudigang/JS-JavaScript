379703db9f3a027e0068f5bf2d96bbc7fc3394f6
gudigang <gugaffaria7@hotmail.com>
2019-11-25 14:56:30 -0300

    Conceitos Básicos

---
+++ script.js
@@ -0,0 +1,55 @@
+//Variáveis
+
+//var mensagem = "Olá Mundo";
+//alert(mensagem);
+
+//Operações
+
+//var a = 2;
+//var b = 3;
+//var c = "Olá Mundo";
+
+//var soma = a+b;
+//var sub = a-b;
+//var mult = a*b;
+//var div = a/b;
+
+//if (c%2 == 1){
+//    alert("Numero Ímpar");
+//}
+//else if (c%2 == 0){
+//      alert("Numero Par");
+//}
+//else{
+//    alert("Valor Invalido");
+//}
+
+//Repetições While(enquanto)
+
+//var i = 0;
+
+//while (i < 3){
+//    alert(i);
+
+//    i = i+1;
+//}
+
+//Repetições For(para)
+
+//for (var j = 0; j < 3; j++){
+//    alert(j);
+//}
+
+//Tipos de Dados
+
+//var inteiro = 6;
+//var decimal = 4.5;
+//var string = "Olá Mundo";
+//var arrays = ["laranja", " maçã", " banana", 1234];
+
+//for (i in arrays){
+//    alert(arrays[i]);
+//}
+
+
+
